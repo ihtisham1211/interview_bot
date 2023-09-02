02750b4d073fb3e158d16343d82270524ed9c95b
ihtisham arif <im.ihtisham.arif@gmail.com>
2023-09-02 15:27:19 +0500

    Add files via upload

---
+++ api/index.js
@@ -0,0 +1,18 @@
+import 'dotenv/config';
+import cors from 'cors';
+import express from 'express';
+import { sendPeriodicMessage } from './bot';
+const cron = require('node-cron');
+
+const app = express();
+app.use(cors());
+
+cron.schedule('*/1 * * * *', sendPeriodicMessage);
+
+app.get('/', (req, res) => {
+  res.status(200).json({ message: 'Live!' });
+});
+
+app.listen(process.env.PORT, () =>
+  console.log('Application listening.....'),
+);
